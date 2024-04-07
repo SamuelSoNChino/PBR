cc5bde7b52525beabb91b8cbeffd596ab09b0f7c
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-04-07 17:32:10 +0200

    Scale and camer fix + Added Shuffle

--- PUZZLE BATTLE ROYALE/Assets/PuzzleGenerator.cs
+++ PUZZLE BATTLE ROYALE/Assets/PuzzleGenerator.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleGenerator : MonoBehaviour
 {
+    public GameObject main_camera;
     public Texture2D skin;
     public Texture2D pad;
     public int pieces;
@@ -15,6 +14,7 @@ public class PuzzleGenerator : MonoBehaviour
     void Start()
     {
         generateGridTiles();
+        adjustCamera();
         shuffleTiles();
     }
 
@@ -31,23 +31,21 @@ public class PuzzleGenerator : MonoBehaviour
                 
 
                 GameObject tile = new GameObject();
-                tile.transform.position = new Vector3(i, j, 0);
-                tile.transform.localScale = new Vector3(2.5f, 2.5f, 1f);
+                tile.transform.position = new Vector3(startW, startH, 0);
                 tile.name = "Tile-" + i.ToString() + "-" + j.ToString();
                 tile.transform.parent = Tiles.transform;
 
-                Texture2D tileSkin = new Texture2D(tileWidth, tileHeight);
+                Texture2D tileSkin  = new Texture2D(tileWidth, tileHeight);
                 tileSkin.SetPixels(skin.GetPixels(startW, startH, tileWidth, tileHeight));
                 tileSkin.Apply();
-                Sprite tileSprite = Sprite.Create(tileSkin, new Rect(0, 0, tileWidth, tileHeight), Vector2.zero, 100);
+                Sprite tileSprite = Sprite.Create(tileSkin, new Rect(0, 0, tileWidth, tileHeight), Vector2.zero, 1);
                 SpriteRenderer spriteRenderer = tile.AddComponent<SpriteRenderer>();
                 spriteRenderer.sprite = tileSprite;
                 spriteRenderer.sortingLayerName = "Tiles";
 
 
                 GameObject gridTile = new GameObject();
-                gridTile.transform.position = new Vector3(i, j, 0);
-                gridTile.transform.localScale = new Vector3(2.5f, 2.5f, 1f);
+                gridTile.transform.position = new Vector3(startW, startH, 0);
                 gridTile.name = "Grid-" + i.ToString() + "-" + j.ToString();
                 gridTile.transform.parent = Grid.transform;
                 
@@ -55,21 +53,32 @@ public class PuzzleGenerator : MonoBehaviour
                 Texture2D gridSkin = new Texture2D(tileWidth, tileHeight);
                 gridSkin.SetPixels(pad.GetPixels(startW, startH, tileWidth, tileHeight));
                 gridSkin.Apply();
-                Sprite gridSprite = Sprite.Create(gridSkin, new Rect(0, 0, tileWidth, tileHeight), Vector2.zero, 100);
+                Sprite gridSprite = Sprite.Create(gridSkin, new Rect(0, 0, tileWidth, tileHeight), Vector2.zero, 1);
                 spriteRenderer = gridTile.AddComponent<SpriteRenderer>();
                 spriteRenderer.sprite = gridSprite;
                 spriteRenderer.sortingLayerName = "Grid";
             }
         }
     }
+    
+    void adjustCamera()
+    {
+        Camera.main.orthographicSize = skin.height;
+        Camera.main.transform.position = new Vector3(skin.width / 2, 0, -1);
+    }
+    
     void shuffleTiles()
     {
-        int numberOfTiles = Tiles.transform.childCount;
+        int tileWidth = skin.width / pieces;
+        float minX = 0;
+        float maxX = skin.width - tileWidth;
+        float minY = -skin.height;
+        float maxY = 0 - tileWidth;
+        float numberOfTiles = Tiles.transform.childCount;
         for (int i = 0; i < numberOfTiles; i++)
         {
-            
             Transform tile = Tiles.transform.GetChild(i);
-            
+            tile.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
         }
     }
 }
