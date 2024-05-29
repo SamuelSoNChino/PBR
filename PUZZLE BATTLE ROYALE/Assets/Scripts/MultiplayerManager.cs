d92da9437e55b6500581e6030b702f130b7f92df
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-05-29 11:47:53 +0200

    Moved GameManager into MultiplayerManger

--- PUZZLE BATTLE ROYALE/Assets/Scripts/MultiplayerManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/MultiplayerManager.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] NetworkManager networkManager;
+    [SerializeField] string serverUrl;
+
     void Start()
     {
-        
+        StartCoroutine(StartGame());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator StartGame()
     {
-        
+        string url = "SamuelSoNChino.eu.pythonanywhere.com";
+        string ip = GetLocalIPAddress();
+        UnityWebRequest matchRequest = UnityWebRequest.Get($"{url}/request_image?{ip}");
+        yield return matchRequest.SendWebRequest();
+        if (matchRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Failed to request a match.");
+            yield break;
+        }
+        string instruction = matchRequest.downloadHandler.text;
+        if (instruction == "WAIT")
+        {
+            networkManager.StartHost();
+        }
+        else
+        {
+            networkManager.GetComponent<UnityTransport>().ConnectionData.Address = instruction;
+            networkManager.StartClient();
+        }
+    }
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("Couldn't find local IP address!");
+    }
+
+    void EndGame()
+    {
+        // TODO
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
