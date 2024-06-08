6d2b034f256d4eae8c1960549633210d8ebd988f
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-06-08 07:01:08 +0200

    Added documentation

--- PUZZLE BATTLE ROYALE/Assets/Scripts/ConnectionTestManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/ConnectionTestManager.cs
@@ -12,13 +12,16 @@ public class ConnectionTestManager : MonoBehaviour
 
     private void Awake()
     {
+        // If wasn't initiaized yet
         if (instance == null)
         {
+            // Sets the instance to this and apply DontDestroyonLoad
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
         else
         {
+            // Destroy the object trying to bocome ConnectionTestManagaer, since one already exists
             Destroy(gameObject);
             return;
         }
@@ -26,27 +29,44 @@ public class ConnectionTestManager : MonoBehaviour
 
     private void Start()
     {
+        // Tests the connection right at the start of the game
         TestConnection();
 
+        // Repeats the connection test every 5 seconds
         InvokeRepeating(nameof(TestConnection), 5f, 5f);
     }
 
     public void TestConnection()
     {
+        // Starts the coroutine for testing connection
         StartCoroutine(TestConnectionCoroutine());
     }
 
     private IEnumerator TestConnectionCoroutine()
     {
+        // Sends a request to the python server to test connection
         UnityWebRequest connectionTestRequest = UnityWebRequest.Get($"{serverUrl}/test_connection");
         yield return connectionTestRequest.SendWebRequest();
 
+        // If the request wasn't successful, loads the connection error screen and breaks the coroutine
         if (connectionTestRequest.result != UnityWebRequest.Result.Success)
         {
             connectionErrorScreen.SetActive(true);
             yield break;
         }
-        connectionErrorScreen.SetActive(false);
-        Debug.Log("Successfully tested connection to the server");
+
+        // Downloads the response from the server
+        string response = connectionTestRequest.downloadHandler.text;
+
+        // If the response was "OK", hides te connection error screen, otherwise loads it
+        if (response == "OK")
+        {
+            connectionErrorScreen.SetActive(false);
+            Debug.Log("Successfully tested connection to the server");
+        }
+        else
+        {
+            connectionErrorScreen.SetActive(true);
+        }
     }
 }
