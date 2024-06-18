22eeb774596c8e9357d8305991a9cf964061bf94
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-06-18 11:16:15 +0200

    Added documentation

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Singleplayer/BackgroundManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Singleplayer/BackgroundManager.cs
@@ -1,37 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UI;
 
+/// <summary>
+/// Manages the loading of the background skin.
+/// </summary>
 public class BackgroundManager : NetworkBehaviour
 {
-
+    /// <summary>
+    /// The background GameObject in the scene.
+    /// </summary>
     [SerializeField] private GameObject background;
+
+    /// <summary>
+    /// Array of available background skins.
+    /// </summary>
     [SerializeField] private Sprite[] backgroundSkins;
 
-    private Dictionary<ulong, int> playerBackgrounds = new();
+    /// <summary>
+    /// Loads the selected background on Awake to ensure PanZoom calculates borders correctly on Start.
+    /// </summary>
     private void Awake()
     {
         LoadOriginalBackground();
     }
 
+    /// <summary>
+    /// Loads the original background skin based on player preferences.
+    /// </summary>
     public void LoadOriginalBackground()
     {
+        // If background wasn't selected in options yet, selects a default value
         if (!PlayerPrefs.HasKey("backgroundSkin"))
         {
             PlayerPrefs.SetInt("backgroundSkin", 0);
         }
-
-        Sprite chosenBackground = backgroundSkins[PlayerPrefs.GetInt("backgroundSkin")];
-        SpriteRenderer backgroundSpriteRenderer = background.GetComponent<SpriteRenderer>();
-        backgroundSpriteRenderer.sprite = chosenBackground;
-    }
-
-    public void LoadNewBackground(int newBackground)
-    {
-        Sprite chosenBackground = backgroundSkins[newBackground];
-        SpriteRenderer backgroundSpriteRenderer = background.GetComponent<SpriteRenderer>();
-        backgroundSpriteRenderer.sprite = chosenBackground;
     }
 }
