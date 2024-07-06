779faae9c5eb2f5203504dc347623dcf4444604e
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-07-06 10:40:39 +0200

    Added documentation

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/PlayerManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -1,22 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// Manages player-related operations such as adding new players and retrieving player information.
+/// </summary>
 public class PlayerManager : MonoBehaviour
 {
+    /// <summary>
+    /// A list to store the players.
+    /// </summary>
     private List<Player> players = new();
 
+    /// <summary>
+    /// Adds a new player to the list of players.
+    /// </summary>
+    /// <param name="newPlayer">The new player to add.</param>
     public void AddNewPlayer(Player newPlayer)
     {
         players.Add(newPlayer);
     }
 
+    /// <summary>
+    /// Retrieves a list of all players.
+    /// </summary>
+    /// <returns>A list of all players.</returns>
     public List<Player> GetAllPlayers()
     {
         return new List<Player>(players);
     }
 
+    /// <summary>
+    /// Finds a player by their client ID.
+    /// </summary>
+    /// <param name="clientId">The client ID of the player to find.</param>
+    /// <returns>The player with the specified client ID, or null if not found.</returns>
     public Player FindPlayerByClientId(ulong clientId)
     {
         foreach (Player player in players)
@@ -28,5 +45,4 @@ public class PlayerManager : MonoBehaviour
         }
         return null;
     }
-
 }
