3067932a712390fb8051e2fcbbf96a0b459bc113
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-07-23 18:48:18 +0200

    Removed snapping and selection, add held tiles

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/PuzzleTileMulitplayer.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/PuzzleTileMulitplayer.cs
@@ -76,72 +76,12 @@ public class PuzzleTileMultiplayer : MonoBehaviour
     }
 
     /// <summary>
-    /// Stores the original position after clicking the tile to determine if the tile should get selected.
-    /// </summary>
-    private Vector3 originalPosition;
-
-    /// <summary>
     /// Gets the current position of the puzzle tile.
     /// </summary>
     public Vector3 Position
     {
         get { return transform.position; }
         set { transform.position = value; }
-    }
-
-    // -----------------------------------------------------------------------
-    // Selection
-    // -----------------------------------------------------------------------
-
-    /// <summary>
-    /// Holds the value of whether the tile is selected for multi-dragging.
-    /// </summary>
-    private bool isSelected = false;
-
-    /// <summary>
-    /// Gets or sets the selection status of the tile.
-    /// </summary>
-    public bool IsSelected
-    {
-        get { return IsSelected; }
-        set
-        {
-            isSelected = value;
-            // Selection is temporarily visualized by changing the scale
-            if (value)
-            {
-                transform.localScale = new Vector3(0.95f, 0.95f, 1);
-            }
-            else
-            {
-                new Vector3(1, 1, 1);
-            }
-        }
-    }
-
-    // -----------------------------------------------------------------------
-    // Grid and Snapping
-    // -----------------------------------------------------------------------
-
-    /// <summary>
-    /// The grid tile the puzzle tile is snapped to (placed on).
-    /// </summary>
-    private GridTileMultiplayer snappedGridTile;
-
-    /// <summary>
-    /// Gets or sets the grid tile the puzzle tile is snapped to.
-    /// </summary>
-    public GridTileMultiplayer SnappedGridTile
-    {
-        get { return snappedGridTile; }
-        set
-        {
-            snappedGridTile = value;
-            if (snappedGridTile != null)
-            {
-                snappedGridTile.IsOccupied = true;
-            }
-        }
     }
 
     // -----------------------------------------------------------------------
@@ -158,12 +98,6 @@ public class PuzzleTileMultiplayer : MonoBehaviour
         Vector3 movedPosition = new(newPosition.x + offset.x, newPosition.y + offset.y, transform.position.z);
         Position = movedPosition;
         puzzleManager.UpdateServerPosition(TileId, movedPosition);
-
-        // Unsnaps itself from the grid tile
-        if (snappedGridTile != null)
-        {
-            UnsnapFromGrid();
-        }
     }
 
     /// <summary>
@@ -191,48 +125,6 @@ public class PuzzleTileMultiplayer : MonoBehaviour
         // Moves this puzzle tile to the front spot (z=1)
         Position = new(transform.position.x, transform.position.y, 1);
         puzzleManager.UpdateServerPosition(TileId, Position);
-    }
-
-    // -----------------------------------------------------------------------
-    // Snapping
-    // -----------------------------------------------------------------------
-
-    /// <summary>
-    /// Snaps the puzzle tile to the grid tile containing the puzzle tile's center, if there is any.
-    /// </summary>
-    public void SnapToGrid()
-    {
-        SpriteRenderer tileSRenderer = GetComponent<SpriteRenderer>();
-        Transform grid = GameObject.Find("Grid").transform;
-        float gridZ = grid.GetChild(0).position.z;
-
-        // Uses Z value of the grid so "Contains" method can be used later
-        Vector3 tileCenter = new(tileSRenderer.bounds.center.x, tileSRenderer.bounds.center.y, gridZ);
-
-        foreach (GameObject gridTileObejct in gridManager.GetAllGridTiles())
-        {
-            SpriteRenderer gridSRenderer = gridTileObejct.GetComponent<SpriteRenderer>();
-            GridTileMultiplayer gridTile = gridTileObejct.GetComponent<GridTileMultiplayer>();
-
-            if (gridSRenderer.bounds.Contains(tileCenter) && !gridTile.IsOccupied)
-            {
-                Move(new Vector3(gridTile.Position.x, gridTile.Position.y, Position.z));
-                SnappedGridTile = gridTile;
-                break;
-            }
-        }
-    }
-
-    /// <summary>
-    /// Clears the snapped grid tile, setting its status to unoccupied.
-    /// </summary>
-    public void UnsnapFromGrid()
-    {
-        if (snappedGridTile != null)
-        {
-            snappedGridTile.IsOccupied = false;
-        }
-        snappedGridTile = null;
     }
 
     // -----------------------------------------------------------------------
@@ -267,19 +159,11 @@ public class PuzzleTileMultiplayer : MonoBehaviour
         if (Input.touchCount == 1 || Input.GetMouseButtonDown(0)) // 2nd condition only for PC testing
         {
             MoveToFront();
-            originalPosition = transform.position;
 
             // Lets the PanZoom script know, that a tile is being dragged, so the player doesn't also pan ath the same time
-            Camera.main.GetComponent<PanZoom>().SetAnyTileDragging(true);
-
-            if (isSelected)
-            {
-                tilesManager.CalculateAllMouseOffsets();
-            }
-            else
-            {
-                CalculateMouseOffset();
-            }
+            Camera.main.GetComponent<PanZoom>().SetHoldingTile(true);
+            
+            CalculateMouseOffset();
         }
     }
 
@@ -290,20 +174,8 @@ public class PuzzleTileMultiplayer : MonoBehaviour
     {
         if (Input.touchCount == 1 || Input.GetMouseButton(0)) // 2nd condition only for PC testing
         {
-            if (isSelected)
-            {
-                tilesManager.MoveSelectedWithMouse();
-            }
-            else
-            {
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Move(mousePosition, mouseOffset);
-            }
-        }
-        // When moving a not selected tile deselect all the tiles
-        if (!isSelected && originalPosition != transform.position)
-        {
-            tilesManager.DeselectAllTiles();
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Move(mousePosition, mouseOffset);
         }
     }
 
@@ -312,24 +184,9 @@ public class PuzzleTileMultiplayer : MonoBehaviour
     /// </summary>
     private void OnMouseUp()
     {
-        // Lets the PanZoom script know, that a tile is not being dragged anymore
-        Camera.main.GetComponent<PanZoom>().SetAnyTileDragging(false);
+        puzzleManager.StopHoldingTile(tileId);
 
-        // Triggers only when the tile was clicked without changing position
-        if (transform.position == originalPosition)
-        {
-            isSelected = !isSelected;
-        }
-        else
-        {
-            if (isSelected)
-            {
-                tilesManager.SnapAllToGrid();
-            }
-            else
-            {
-                SnapToGrid();
-            }
-        }
+        // Lets the PanZoom script know, that a tile is not being dragged anymore
+        Camera.main.GetComponent<PanZoom>().SetHoldingTile(false);
     }
 }
