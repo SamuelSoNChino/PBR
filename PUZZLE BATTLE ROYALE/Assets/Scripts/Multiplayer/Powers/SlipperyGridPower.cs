76e1a669749d72ef9bc702e6731a34c2e67dc63f
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-08-16 17:17:28 +0200

    Create SlipperyGridPower.cs

---
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/SlipperyGridPower.cs
@@ -0,0 +1,12 @@
+public class SlipperyGridPower : Power
+{
+    public SlipperyGridPower() : base("Slippery Grid", 0, false, true, 1)
+    {
+
+    }
+
+    public override void Activate()
+    {
+
+    }
+}
