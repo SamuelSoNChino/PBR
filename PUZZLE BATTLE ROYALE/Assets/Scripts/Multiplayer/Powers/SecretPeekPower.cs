d0472c84e3908ad5d89fe23a1fd4e2a61def5e15
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-08-16 17:17:21 +0200

    Create SecretPeekPower.cs

---
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/SecretPeekPower.cs
@@ -0,0 +1,12 @@
+public class SecretPeekPower : Power
+{
+    public SecretPeekPower() : base("Secret Peek", 0, false, true, 1)
+    {
+
+    }
+
+    public override void Activate()
+    {
+
+    }
+}
