0723b012f32102d71e6eb0032c66fe8d75f5a3a3
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-08-20 20:36:06 +0200

    MOdified some attributes

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/Power.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/Power.cs
@@ -1,17 +1,15 @@
-using UnityEngine;
-
 public abstract class Power
 {
     public string Name { get; private set; }
-    public int IconId { get; private set; }
+    public int Id { get; private set; }
     public bool IsPassive { get; private set; }
     public bool IsTargetable { get; private set; }
     public int NumberOfTargets { get; private set; }
 
-    protected Power(string name, int iconId, bool isPassive, bool isTargetable, int numberOfTargets)
+    protected Power(string name, int id, bool isPassive, bool isTargetable, int numberOfTargets)
     {
         Name = name;
-        IconId = iconId;
+        Id = id;
         IsPassive = isPassive;
         IsTargetable = isTargetable;
         NumberOfTargets = numberOfTargets;
