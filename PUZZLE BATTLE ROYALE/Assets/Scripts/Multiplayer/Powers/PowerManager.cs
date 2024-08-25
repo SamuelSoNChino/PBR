29cc82c50ca154931ba723c869248f974f9efba0
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-08-25 15:27:58 +0200

    Basically added whole functionality

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/PowerManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/PowerManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
@@ -6,21 +7,19 @@ using UnityEngine.UI;
 
 public class PowerManager : NetworkBehaviour
 {
-    // -----------------------------------------------------------------------
-    // UNIVERSAL
-    // -----------------------------------------------------------------------
+    [SerializeField] private PlayerManager playerManager;
+
 
     private void Start()
     {
         LoadEquippedPowers();
-        loadoutManager.UpdateLoadout();
     }
 
     // -----------------------------------------------------------------------
     // Powers
     // -----------------------------------------------------------------------
 
-    private readonly List<Power> powers = new List<Power>
+    private readonly List<Power> powers = new()
     {
         new TornadoPower(),
         new SecretPeekPower(),
@@ -57,44 +56,12 @@ public class PowerManager : NetworkBehaviour
         return null;
     }
 
-
-
-    // -----------------------------------------------------------------------
-    // CLIENT
-    // -----------------------------------------------------------------------
-
-    // -----------------------------------------------------------------------
-    // Power Buttons and Sprites
-    // -----------------------------------------------------------------------
-
     [SerializeField] private List<Sprite> powerIconsSprites;
-    private List<Button> powerButtons;
 
     public Sprite GetPowerSprite(Power power)
     {
         return powerIconsSprites[power.Id];
     }
-
-    private void FindPowerButtons()
-    {
-        for (int i = 0; i < numberOfPowerSlots; i++)
-        {
-            powerButtons.Add(GameObject.Find($"PowerButton{i + 1}").GetComponent<Button>());
-        }
-    }
-
-    // [Rpc(SendTo.ClientsAndHost)]
-    // public void LoadEquippedPowersSpritesRpc(ulong clientId, List<int> equippedPowersSpritesIds)
-    // {
-    //     if (true)
-    //     {
-
-    //     }
-    //     for (int i = 0; i < equippedPowersSpritesIds.Count; i++)
-    //     {
-
-    //     }
-    // }
 
     // -----------------------------------------------------------------------
     // Equipped Powers
@@ -106,19 +73,6 @@ public class PowerManager : NetworkBehaviour
         get { return equippedPowers; }
     }
     private readonly int numberOfPowerSlots = 3;
-
-    public void EquipPower(int powerButtonIndex, Power newPower)
-    {
-        equippedPowers.RemoveAt(powerButtonIndex);
-        equippedPowers.Insert(powerButtonIndex, newPower);
-        PlayerPrefs.SetInt($"equippedPower{powerButtonIndex}Id", newPower.Id);
-    }
-
-    // -----------------------------------------------------------------------
-    // Loadout functionality
-    // -----------------------------------------------------------------------
-
-    [SerializeField] private LoadoutManager loadoutManager;
 
     private void LoadEquippedPowers()
     {
@@ -149,24 +103,162 @@ public class PowerManager : NetworkBehaviour
         }
     }
 
+    [Rpc(SendTo.Server)]
+    public void SendEquippedPowersToServerRpc(ulong clientId, string serializedEquippedPowersIds)
+    {
+        Player player = playerManager.FindPlayerByClientId(clientId);
 
+        List<string> equippedPowersIds = new(serializedEquippedPowersIds.Split(","));
+
+        foreach (string powerIdString in equippedPowersIds)
+        {
+            int powerId = int.Parse(powerIdString);
+            Power power = FindPowerById(powerId);
+            if (!player.HasPower(power) && powers.Contains(power) && player.Powers.Count != numberOfPowerSlots)
+            {
+                player.AddPower(power);
+            }
+        }
+    }
+
+    private void SendEquippedPowersToServer()
+    {
+        List<int> equippedPowersIds = new();
+        foreach (Power power in equippedPowers)
+        {
+            equippedPowersIds.Add(power.Id);
+        }
+        string serializedEquippedPowersIds = string.Join(",", equippedPowersIds);
+        SendEquippedPowersToServerRpc(NetworkManager.Singleton.LocalClientId, serializedEquippedPowersIds);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    public void RequestEquippedPowersFromAllPlayersRpc()
+    {
+        SendEquippedPowersToServer();
+    }
 
     // -----------------------------------------------------------------------
-    // SERVER
+    // Power Buttons
     // -----------------------------------------------------------------------
 
-    [SerializeField] private PlayerManager playerManager;
+    [SerializeField] private GameObject powerButtons;
+    [SerializeField] private PeekManager peekManager;
 
-    // [Rpc(SendTo.Server)]
-    // public void SendEquippedPowersToServerRpc(ulong clientId, List<Power> equippedPowers)
-    // {
-    //     Player player = playerManager.FindPlayerByClientId(clientId);
-    //     foreach (Power power in equippedPowers)
-    //     {
-    //         if (!player.HasPower(power) && powers.Contains(power) && player.Powers.Count != 3)
-    //         {
-    //             player.AddPower(power);
-    //         }
-    //     }
-    // }
+    [Rpc(SendTo.ClientsAndHost)]
+    private void InitializePowerButtonsRpc(ulong clientId, string serializedPowersIds)
+    {
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            List<string> powersIds = new(serializedPowersIds.Split(","));
+            for (int i = 0; i < powerButtons.transform.childCount; i++)
+            {
+                Power power = FindPowerById(int.Parse(powersIds[i]));
+                Sprite powerSprite = GetPowerSprite(power);
+                powerButtons.transform.GetChild(i).GetComponent<Image>().sprite = powerSprite;
+
+                if (power.IsPassive || power.IsTargetable)
+                {
+                    powerButtons.transform.GetChild(i).GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+
+    public void InitializePowerButtonsForAllPlayers()
+    {
+        foreach (Player player in playerManager.GetAllPlayers())
+        {
+            List<int> powerIds = new();
+            foreach (Power power in player.Powers)
+            {
+                powerIds.Add(power.Id);
+
+                if (!power.IsPassive)
+                {
+                    OnPowerCooldownStatusChange += UpdatePowerButtonInteractability;
+                    peekManager.OnPlayerPeekingStatusChanged += UpdatePowerButtonInteractability;
+                }
+            }
+            string serializedPowersIds = string.Join(",", powerIds);
+            InitializePowerButtonsRpc(player.ClientId, serializedPowersIds);
+        }
+    }
+
+    public void UpdatePowerButtonInteractability(Player player)
+    {
+        for (int i = 0; i < player.Powers.Count; i++)
+        {
+            Power power = player.GetPowerAtIndex(i);
+            if (NetworkManager.Singleton.IsServer)
+            {
+                Debug.Log($"{power.Name}: isOnCooldown = {player.IsPowerOnCooldown(power)}");
+                Debug.Log($"{power.Name}: isPassive = {power.IsPassive}");
+                Debug.Log($"{power.Name}: IsTargetable = {power.IsTargetable}");
+                Debug.Log($"Player: IsPeeking = {player.IsPeeking}");
+            }
+            if (power.IsPassive)
+            {
+                continue;
+            }
+
+            if (!player.IsPowerOnCooldown(power) && !(power.IsTargetable && !player.IsPeeking))
+            {
+                EnablePowerButtonRpc(player.ClientId, i);
+                continue;
+            }
+            DisablePowerButtonRpc(player.ClientId, i);
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    public void DisablePowerButtonRpc(ulong clientId, int powerButtonIndex)
+    {
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            powerButtons.transform.GetChild(powerButtonIndex).GetComponent<Button>().interactable = false;
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    public void EnablePowerButtonRpc(ulong clientId, int powerButtonIndex)
+    {
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            powerButtons.transform.GetChild(powerButtonIndex).GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void UsePowerButton(int buttonIndex)
+    {
+        UsePowerButtonRpc(NetworkManager.Singleton.LocalClientId, buttonIndex);
+    }
+
+    [Rpc(SendTo.Server)]
+    public void UsePowerButtonRpc(ulong clientId, int buttonIndex)
+    {
+        Player player = playerManager.FindPlayerByClientId(clientId);
+        Power power = player.GetPowerAtIndex(buttonIndex);
+
+        if (!power.IsPassive && !player.IsPowerOnCooldown(power) && !(power.IsTargetable && !player.IsPeeking))
+        {
+            power.Activate();
+            StartCoroutine(StartPowerCooldownTimer(player, power));
+        }
+    }
+
+    // -----------------------------------------------------------------------
+    // Power Cooldowns
+    // -----------------------------------------------------------------------
+
+    public event Action<Player> OnPowerCooldownStatusChange;
+
+    public IEnumerator StartPowerCooldownTimer(Player player, Power power)
+    {
+        player.PutPowerOnCooldown(power);
+        OnPowerCooldownStatusChange.Invoke(player);
+        yield return new WaitForSeconds(power.CooldownDuration);
+        player.PutPowerOffCooldown(power);
+        OnPowerCooldownStatusChange.Invoke(player);
+    }
 }
