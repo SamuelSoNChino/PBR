d8b420cd163312b8a274150558cf225c22b1fbaa
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-08-29 08:26:36 +0200

    Added documentation

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/CoroutineHelper.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/CoroutineHelper.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// A helper class for managing coroutines. This class ensures that coroutines can be started and stopped from non-MonoBehaviour classes.
+/// </summary>
 public class CoroutineHelper : MonoBehaviour
 {
     private static CoroutineHelper instance;
+
+    /// <summary>
+    /// Gets the singleton instance of the <see cref="CoroutineHelper"/> class.
+    /// </summary>
     public static CoroutineHelper Instance
     {
         get
@@ -17,11 +24,20 @@ public class CoroutineHelper : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Starts a coroutine using the specified enumerator.
+    /// </summary>
+    /// <param name="routine">The enumerator to start as a coroutine.</param>
+    /// <returns>The coroutine that was started.</returns>
     public Coroutine StartHelperCoroutine(IEnumerator routine)
     {
         return StartCoroutine(routine);
     }
 
+    /// <summary>
+    /// Stops the specified coroutine.
+    /// </summary>
+    /// <param name="coroutine">The coroutine to stop.</param>
     public void StopHelperCoroutine(Coroutine coroutine)
     {
         StopCoroutine(coroutine);
