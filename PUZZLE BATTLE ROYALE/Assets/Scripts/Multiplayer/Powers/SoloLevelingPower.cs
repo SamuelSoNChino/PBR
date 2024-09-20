2fe15b09dea4e62e612bbcba1f71f844d5793a0c
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-09-20 16:41:54 +0200

    adjusted docnumentation

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/SoloLevelingPower.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/SoloLevelingPower.cs
@@ -1,5 +1,6 @@
 /// <summary>
-/// Represents the "Solo Leveling" power which enables player play without.
+/// Represents the "Solo Leveling" power which makes the player ranking invisible to all people and also disables the player for using 
+/// peek or being the target of peek.
 /// </summary>
 public class SoloLevelingPower : Power
 {
