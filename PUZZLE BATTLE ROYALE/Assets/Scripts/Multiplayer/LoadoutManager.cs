83281c354212db980d6461aca8f94d1f1ce7b8a9
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-10-06 20:05:59 +0200

    Added BerserkPower

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/LoadoutManager.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/LoadoutManager.cs
@@ -37,6 +37,7 @@ public class LoadoutManager : MonoBehaviour
         new ShieldPower(),
         new SlipperyGridPower(),
         new SoloLevelingPower(),
+        new BerserkPower()
     };
 
     /// <summary>
