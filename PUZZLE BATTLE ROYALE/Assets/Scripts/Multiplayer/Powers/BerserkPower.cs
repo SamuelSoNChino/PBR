c45bbb53ede2a028b2e8944b92a8b73bc4b22dbc
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-10-06 14:38:48 +0200

    Prepared for coding

--- PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/BerserkPower.cs
+++ PUZZLE BATTLE ROYALE/Assets/Scripts/Multiplayer/Powers/BerserkPower.cs
@@ -11,40 +11,23 @@ public class BerserkPower : Power
     /// <summary>
     /// Initializes a new instance of the <see cref="BerserkPower"/> class.
     /// </summary>
-    public SlipperBerserkPoweryGridPower() : base("Berserk", 3, false, true, 5)
+    public BerserkPower() : base("Berserk", 5, false, true, 5)
     {
     }
 
     /// <summary>
-    /// A dictionary that keeps track of active coroutines associated with each player.
-    /// </summary>
-    private Dictionary<Player, Coroutine> activeCoroutines = new();
-
-    /// <summary>
-    /// Activates the Berserk power during a peek.
+    /// Activates the Berserk power.
     /// </summary>
     /// <param name="targetPlayer">The player on whom the power is activated.</param>
-    public override void Activate(Player targetPlayer)
+    public override void Activate(Player userPlayer, Player targetPlayer)
     {
-        if (activeCoroutines.Keys.Contains(targetPlayer))
-        {
-            CoroutineHelper.Instance.StopHelperCoroutine(activeCoroutines[targetPlayer]);
-        }
-
-        Coroutine newCoroutine = CoroutineHelper.Instance.StartHelperCoroutine(UseTimer(targetPlayer, 5));
-        activeCoroutines[targetPlayer] = newCoroutine;
+        
     }
 
-    /// <summary>
-    /// A coroutine that disables the player's ability to snap to the grid for a specified duration.
-    /// </summary>
-    /// <param name="targetPlayer">The player whose grid snapping is disabled.</param>
-    /// <param name="useDuration">The duration for which grid snapping is disabled.</param>
-    /// <returns>An enumerator for the coroutine.</returns>
     private IEnumerator UseTimer(Player targetPlayer, float useDuration)
     {
-        targetPlayer.SnapToGridEnabled = false;
+        
         yield return new WaitForSeconds(useDuration);
-        targetPlayer.SnapToGridEnabled = true;
+        
     }
 }
